4d53d199197ac425937de8ae2c52c3d4bc57c52c
Alaa ashraf mahmoud ali <99879452+AlaaAlq99@users.noreply.github.com>
2025-06-10 22:41:47 +0300

    Update Describtion.h

--- Floyd’s Cycle Detection/Describtion.h
+++ Floyd’s Cycle Detection/Describtion.h
@@ -1,1 +1,21 @@
 
+This contains two implementations of Floyd’s Cycle Detection Algorithm (also known as the Tortoise and Hare Algorithm) using singly linked lists and Sequence.
+
+✅ Features:
+Detects if a cycle exists .
+Calculates the length of the cycle.
+Finds the starting node of the cycle.
+
+
+📚 Usage:
+This algorithm is very efficient:
+Time Complexity: O(n)
+Space Complexity: O(1)
+(Does not require extra memory like sets or maps)
+
+📌 Applications:
+Detecting infinite loops in data structures
+Finding cycles in linked list-based problems
+Efficient memory/scheduling in OS & networks
+
+
